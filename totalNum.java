6d6c9941eb48a6eadd6c37ce17bdce0fc1990349
Spr_zero <81459719+nk712001@users.noreply.github.com>
2021-11-28 21:17:58 +0530

    Add files via upload

---
+++ totalNum.java
@@ -0,0 +1,11 @@
+package edyoda7;
+import java.util.*;
+import java.util.stream.*;
+public class totalNum {
+        public static void main(String args[]) {
+            List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+            long count =  myList.stream()
+                    .count();
+            System.out.println(count);
+        }
+}
